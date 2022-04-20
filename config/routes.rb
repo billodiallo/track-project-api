3d35ed2ceb16003392d35c4cc26c2eabef7d993d
Billo Diallo <billodiallonet@gmail.com>
2022-04-20 22:25:29 +0300

    Clean routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :programmers do
-        resources :projects
-    
-      end
-
+      resources :programmers
       resources :projects
-
-      # post '/addit', to: "secondcprojects#create"
-      # get '/display', to: "secondcprojects#index"
-      
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
