01666a6d8347435af3788b1ea823b302a8c1b688
Billo Diallo <billodiallonet@gmail.com>
2022-05-03 15:19:04 +0300

    Add uniqness project title model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   has_many :vendors, dependent: :destroy
   has_many :programmers, through: :vendors, dependent: :destroy
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :startDate, presence: true
   validates :deadLineDate, presence: true
   validates :budget, presence: true
